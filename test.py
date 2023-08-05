39242defe7393621c082566e3e3a57b966b1ad34
GitHub Action <action@github.com>
2023-08-05 13:47:42 +0000

    Replaced old code with new code from API

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 
-def create_large_list():
-    large_list = []
-    for i in range(1000000):
-        large_list.append(i)
-    return large_list
+from functools import lru_cache
 
+@lru_cache(maxsize=None)
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
 
+@lru_cache(maxsize=None)
 def give_output(n):
     if n <= 1:
         return n
     else:
         return give_output(n-1) + give_output(n-2)
+
+def create_large_list():
+    return list(range(1000000))
